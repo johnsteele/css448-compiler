ff804c57989bef4c1b333dbb03807aa657418d1a
John Steele <programjsteele@gmail.com>
2011-05-13 19:13:24 -0700

    Here's the TypeRecord, just paste what you have into it.

---
+++ src/phase3/TypeRecord.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * @file TypeRecord.h                                                         *
+ *                                                                            *
+ * @brief CSS 448 - Compiler Phase 3 - TypeRecord                             *
+ *                                                                            *
+ * @author John Steele      \<steelejr@u.washington.edu\>                     *
+ * @author Alicia Flinchum  \<aliciaflinchum@yahoo.com\>                      *
+ *                                                                            *
+ * @version 1.0.0                                                             *
+ * @date May 1, 2011                                                          *
+ *                                                                            *
+ * @brief TypeRecord represents a generic record type.                        *
+ *****************************************************************************/
+
+#include "TypeRecord.h"
+
+TypeRecord::TypeRecord() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TypeRecord::~TypeRecord() {
+	// TODO Auto-generated destructor stub
+}

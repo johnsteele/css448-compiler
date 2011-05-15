7ead0daf5d78d1d2c893e101e510fff5daeba22a
John Steele <programjsteele@gmail.com>
2011-05-15 15:37:57 -0700

    Replaced access violation with getType.

--- src/phase3/ProcedureRecord.cpp
+++ src/phase3/ProcedureRecord.cpp
@@ -76,7 +76,9 @@ void ProcedureRecord::print (int scope) const {
 	// Print parameters (if there is any).
 	for (int i = 0; i < (long) args->size(); i++) {
 		args->at(i)->print(scope + 1);
-		cout << args->at(i)->getType()->getName();
+		if (args->at(i)->getType() != NULL) {
+			cout << args->at(i)->getType()->getName();
+		}
 		cout << endl;
 	}
 }
